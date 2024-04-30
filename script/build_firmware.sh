12142bd942a057681c00b6d4a27eb902b634560f
pennix <pennaliflake@gmail.com>
2024-04-30 15:45:45 +0800

    changes

--- script/build_firmware.sh
+++ script/build_firmware.sh
@@ -7,13 +7,21 @@ CUR_DIR=$(cd $(dirname $0); pwd)
 UBOOT_DIR=${CUR_DIR}/../src/u-boot
 OPENSBI_DIR=${CUR_DIR}/../src/opensbi
 
+pushd ${UBOOT_DIR}
+git clean -xdf
+popd
+
 pushd ${OPENSBI_DIR}
-make PLATFORM=generic FW_PAYLOAD_PATH=${UBOOT_DIR}/u-boot.bin
+git clean -xdf
 popd
 
 pushd ${UBOOT_DIR}
 make qemu-riscv64_smode_defconfig
 make -j`nproc`
+popd
+
+pushd ${OPENSBI_DIR}
+make PLATFORM=generic FW_PAYLOAD_PATH=${UBOOT_DIR}/u-boot.bin
 popd
 
 export OPENSBI=${OPENSBI_DIR}/build/platform/generic/firmware/fw_dynamic.bin
--- script/run_qemu.sh
+++ script/run_qemu.sh
@@ -6,14 +6,41 @@ UBOOT_DIR=${CUR_DIR}/../src/u-boot
 LINUX_DIR=${CUR_DIR}/../src/linux
 OPENSBI_DIR=${CUR_DIR}/../src/opensbi
 
+
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+# ${QEMU_DIR}/build/qemu-system-riscv64 \
+#     -M virt \
+#     -smp 4 \
+#     -m 4G \
+#     -display none \
+#     -serial stdio \
+#     -bios ${UBOOT_DIR}/spl/u-boot-spl \
+#     -device loader,file=${UBOOT_DIR}/u-boot.itb,addr=0x80200000
+
 ${QEMU_DIR}/build/qemu-system-riscv64 \
     -M virt \
     -smp 4 \
     -m 4G \
     -display none \
     -serial stdio \
-    -bios ${UBOOT_DIR}/spl/u-boot-spl \
-    -device loader,file=${UBOOT_DIR}/u-boot.itb,addr=0x80200000
+    -bios ${OPENSBI_DIR}/build/platform/generic/firmware/fw_jump.bin \
+    -kernel ${UBOOT_DIR}/u-boot.bin
+
+
+# ${QEMU_DIR}/build/qemu-system-riscv64 \
+#     -M virt \
+#     -smp 4 \
+#     -m 4G \
+#     -display none \
+#     -serial stdio \
+#     -bios ${OPENSBI_DIR}/build/platform/generic/firmware/fw_payload.elf
 
 # ${QEMU_DIR}/build/qemu-system-riscv64 \
 #     -M virt \
